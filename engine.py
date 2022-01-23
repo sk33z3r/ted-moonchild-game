f3c80dd0346b9b35041c105e4de533cc6f6b7596
sk33z3r <sk33z3r@protonmail.com>
2022-01-22 19:12:33 -0500

    chore[python]: only center windo if the terminal is large enough, add comments

--- engine.py
+++ engine.py
@@ -35,71 +35,82 @@ def calculateWindows(height, width):
     global msgDims
     global statDims
     global invDims
+    global helpDims
 
-    # TODO set initial height, width, begin_y, begin_x from terminal size
-    initHeight = 0
-    initWidth = 0
-    initBegin_y = round((height - max_y) / 2)
-    initBegin_x = round((width - max_x) / 2)
+    # if the terminal window is bigger than our UI, center everything
+    if max_x < width and max_y < height:
 
-    # section dimension map
-    ### all tuples (height, width, begin_y, begin_x)
-    ### input border tuple (uly, ulx, lry, lrx)
-    sectionDims = {
-        "title": {
-            "border": [ 3, 80, initBegin_y, (initBegin_x + 1) ],
-            "content": [ 1, 76, (initBegin_y + 1), (initBegin_x + 3) ]
-        },
-        "ground": {
-            "border": [ 8, 50, (initBegin_y + 25), (initBegin_x + 1) ],
-            "content": [ 6, 46, (initBegin_y + 26), (initBegin_x + 3) ]
-        },
-        "exits": {
-            "border": [ 8, 29, (initBegin_y + 25), (initBegin_x + 52) ],
-            "content": [ 6, 25, (initBegin_y + 26), (initBegin_x + 53) ]
-        },
-        "events": {
-            "border": [ 22, 80, (initBegin_y + 3), (initBegin_x + 1) ],
-            "content": [ 20, 76, (initBegin_y + 4), (initBegin_x + 3) ]
-        },
-        "input": {
-            "border": [ (initBegin_y + 33), (initBegin_x + 1), (initBegin_y + 35), (initBegin_x + 80) ],
-            "content": [ 1, 72, (initBegin_y + 34), (initBegin_x + len(PROMPT) + 3) ],
-            "prompt": [ (initBegin_y + 34), (initBegin_x + 3) ]
-        },
-        "msg": {
-            "border": [ 4, 109, (initBegin_y + 36), (initBegin_x + 1) ],
-            "content": [ 2, 105, (initBegin_y + 37), (initBegin_x + 3) ]
-        },
-        "stats": {
-            "border": [ 9, 28, initBegin_y, (initBegin_x + 82) ],
-            "content": [ 7, 24, (initBegin_y + 1), (initBegin_x + 84) ]
-        },
-        "inventory": {
-            "border": [ 27, 28, (initBegin_y + 9), (initBegin_x + 82) ],
-            "content": [ 25, 24, (initBegin_y + 10), (initBegin_x + 84) ]
-        },
-        "help": {
-            "border": [],
-            "content": []
+        # determine where curses needs to start the windows to be centered in the terminal
+        initBegin_y = round((height - max_y) / 2)
+        initBegin_x = round((width - max_x) / 2)
+
+        # section dimension map
+        ### all tuples (height, width, begin_y, begin_x)
+        ### input border tuple (uly, ulx, lry, lrx)
+        sectionDims = {
+            "title": {
+                "border": [ 3, 80, initBegin_y, (initBegin_x + 1) ],
+                "content": [ 1, 76, (initBegin_y + 1), (initBegin_x + 3) ]
+            },
+            "ground": {
+                "border": [ 8, 50, (initBegin_y + 25), (initBegin_x + 1) ],
+                "content": [ 6, 46, (initBegin_y + 26), (initBegin_x + 3) ]
+            },
+            "exits": {
+                "border": [ 8, 29, (initBegin_y + 25), (initBegin_x + 52) ],
+                "content": [ 6, 25, (initBegin_y + 26), (initBegin_x + 53) ]
+            },
+            "events": {
+                "border": [ 22, 80, (initBegin_y + 3), (initBegin_x + 1) ],
+                "content": [ 20, 76, (initBegin_y + 4), (initBegin_x + 3) ]
+            },
+            "input": {
+                "border": [ (initBegin_y + 33), (initBegin_x + 1), (initBegin_y + 35), (initBegin_x + 80) ],
+                "content": [ 1, 72, (initBegin_y + 34), (initBegin_x + len(PROMPT) + 3) ],
+                "prompt": [ (initBegin_y + 34), (initBegin_x + 3) ]
+            },
+            "msg": {
+                "border": [ 4, 109, (initBegin_y + 36), (initBegin_x + 1) ],
+                "content": [ 2, 105, (initBegin_y + 37), (initBegin_x + 3) ]
+            },
+            "stats": {
+                "border": [ 9, 28, initBegin_y, (initBegin_x + 82) ],
+                "content": [ 7, 24, (initBegin_y + 1), (initBegin_x + 84) ]
+            },
+            "inventory": {
+                "border": [ 27, 28, (initBegin_y + 9), (initBegin_x + 82) ],
+                "content": [ 25, 24, (initBegin_y + 10), (initBegin_x + 84) ]
+            },
+            "help": {
+                "border": [35, 55, (initBegin_y + 17), (initBegin_x + 27)],
+                "content": [33, 51, (initBegin_y + 18), (initBegin_x + 29)]
+            }
         }
-    }
 
-    # section references
-    titleDims = sectionDims["title"]
-    groundDims = sectionDims["ground"]
-    exitDims = sectionDims["exits"]
-    eventDims = sectionDims["events"]
-    inputDims = sectionDims["input"]
-    msgDims = sectionDims["msg"]
-    statDims = sectionDims["stats"]
-    invDims = sectionDims["inventory"]
+        # section references
+        titleDims = sectionDims["title"]
+        groundDims = sectionDims["ground"]
+        exitDims = sectionDims["exits"]
+        eventDims = sectionDims["events"]
+        inputDims = sectionDims["input"]
+        msgDims = sectionDims["msg"]
+        statDims = sectionDims["stats"]
+        invDims = sectionDims["inventory"]
+        helpDims = sectionDims["help"]
+
+    # otherwise crap out
+    else:
+
+        # raise an exception
+        raise Exception("ERROR: Terminal window should be at least 110 x 70")
 
 # define the PROMPT design
 PROMPT = "\m/: "
 
 # identify the OS
 def identify_os():
+
+    # setup the clear function
     system = sys.platform
     if system == "win32":
         return "cls"
@@ -110,9 +121,12 @@ def identify_os():
 def clear():
     system(identify_os())
 
+# define styles for curses
 def setStyles():
+
     # define globals
     global c
+
     # initialize color pairs
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
@@ -120,6 +134,7 @@ def setStyles():
     curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
     curses.init_pair(6, curses.COLOR_CYAN, curses.COLOR_BLACK)
+
     # color map
     c = {
         "WHITE": curses.color_pair(0),
@@ -159,54 +174,68 @@ def setStyles():
         "BLINK_BRIGHT_CYAN": curses.A_BLINK + curses.A_BOLD + curses.color_pair(6)
     }
 
+# function to refresh info in memory
 def refreshInfo():
     dbs.getInventory()
     dbs.getStats()
     dbs.getLocation()
 
+# get the effects string for inventory and shop printouts
 def getEffectString(item):
+
     # get the name of the item
     itemInfo = dbs.items.find_one( { "NAME": item } )
     effectList = ""
+
     # check if the item has EFFECTs
     try:
         effectList = itemInfo["EFFECT"]
+
+    # if not, build string based on TYPE
     except KeyError:
-        # if not, build string based on TYPE
         if itemInfo["TYPE"] in [ "weapon", "fx" ]:
             effectString = "[+{0}]".format(str(itemInfo["ATKBNS"]))
         else:
             effectString = ""
+
+    # otherwise build the string based on the EFFECT
     else:
-        # build the string based on the EFFECT
         if effectList[0] in [ "DEF", "ATK", "MAG" ]:
             effectString = "[{0}{1}]".format(effectList[0], effectList[1])
         elif effectList[0] in [ "HP", "MP", "XP" ]:
             effectString = "[{0} {1}{2}]".format(effectList[0], effectList[2], str(effectList[1]))
+
+    # return the whole string
     return effectString
 
+# get the first item that matches a descword
 def getFirstItemMatchingDesc(desc, itemList):
-    itemList = list(set(itemList)) # make itemList unique
+
+    # make itemList unique
+    itemList = list(set(itemList))
+
+    # iterate through each item in the given list
     for item in itemList:
+
+        # when the first one is found, return the name
         if desc in dbs.items.find_one( { "NAME": item } )["DESCWORDS"]:
             return item
+
+    # if none are found, return None
     return None
 
-def getRandDescWords(item):
-    # Returns a list of "description words" for each item named in itemList.
-    descWords = []
-    descWords.extend(dbs.items.find_one( { "NAME": item } )["DESCWORDS"])
-    desc = random.randomchoice(descWords)
-    return desc
-
+# get an item's grounddesc
 def getGroundDesc(item):
     desc = dbs.items.find_one( { "NAME": item } )["GROUNDDESC"]
     return desc
 
 # function to get a full list of the current player inventory
 def tempInv():
+
+    # get the latest info
     dbs.getInventory()
-    # build temp inv list
+
+    # build temp inv list from all types
     inv = []
     n = 0
     for i in dbs.playerInv["ITEMS"]:
@@ -221,66 +250,118 @@ def tempInv():
         inv.append(dbs.playerInv["EQUIPPED"][n])
         n += 1
     inv = natsorted(inv)
+
+    # return the list
     return inv
 
 # function to buy and sell items
 def itemTransaction(item, dowhat):
+
+    # if sell is specified
     if dowhat == "sell":
+
+        # reduce the value by 25%
         val = round(dbs.items.find_one( { "NAME": item } )["VALUE"] * 0.75)
+
+        # set message text
         message = [ "Ted sold {0} for {1} FLOYDS.".format(item, str(val)), "GREEN" ]
+
+        # updateplayer's FLOYDS and inventory
         dbs.updateStat("FLOYDS", val, "inc")
         dbs.updateInv(item, "del")
+
+    # if buy is specified
     if dowhat == "buy":
+
+        # get the raw value
         val = dbs.items.find_one( { "NAME": item } )["VALUE"]
+
+        # if the item costs more than player has floyds, set a failure message
         if val > dbs.playerStats["FLOYDS"]:
             message = [ "Ted doesn't have enough FLOYDS to pay for that!", "RED" ]
+
+        # otherwise set a success message
         else:
             message = [ "Ted bought {0} for {1} FLOYDS.".format(item, str(val)), "GREEN" ]
+
+            # update player's FLOYDS and inventory
             dbs.updateStat("FLOYDS", val, "dec")
             dbs.updateInv(item, "add")
+
+    # return set message
     return message
 
+# function to gain XP and check if a level was gained
 def gainXP(val):
+
+    # update the stat in db
     dbs.updateStat("XP", val, "inc")
+
+    # if XP went above the requirement for the next level, do the level increase
     if dbs.playerStats["XP"] >= dbs.levels.find_one( { "LEVEL": dbs.playerStats["LVL"] + 1 } )["XPREQ"]:
-        # if XP went above the requirement for the next level, do the level increase
+
+        # increase LVL
         dbs.updateStat("LVL", 1, "inc")
+
+        # update memory
         dbs.getStats()
+
+        # update remaining stats from the level db
         dbs.updateStat("HPMAX", dbs.levelStats["HPMAX"], "set")
         dbs.updateStat("MPMAX", dbs.levelStats["MPMAX"], "set")
         dbs.updateStat("HP", dbs.levelStats["HPMAX"], "set")
         dbs.updateStat("MP", dbs.levelStats["MPMAX"], "set")
+
+        # reload memory again
         dbs.getStats()
+
+        # set a message
         lvlMsg = "Ted gained a level! {0}".format(" ".join(dbs.levels.find_one( { "LEVEL": dbs.playerStats["LVL"] } )["MSG"]))
+
+    # if no level gain, set blank message
     else:
         lvlMsg = ""
+
+    # return message
     return lvlMsg
 
 # function to use items
 def useItem(name):
+
     # get item info
     itemInfo = dbs.items.find_one( { "NAME": name } )
+
     # figure out what the item is, and do the thing
     if itemInfo["TYPE"] in [ "drug", "food", "smoke", "drink" ]:
+
         # store some vars for later
         lvlMsg = ""
         stat = itemInfo["EFFECT"][0]
         op = itemInfo["EFFECT"][1]
+
+        # if the item has a 3rd effect, then store it
         try:
             val = itemInfo["EFFECT"][2]
+
+        # otherwise make it blank
         except IndexError:
             val = ""
+
+        # set remaining vars
         maxHP = dbs.playerStats["HPMAX"]
         hp = dbs.playerStats["HP"]
         maxMP = dbs.playerStats["MPMAX"]
         mp = dbs.playerStats["MP"]
-        # if the stat is HP or MP, we need to check that the new value isn't above or below the max/min parameters
+
+        # if the effect increases HP, make sure it doesn't go above max
         if stat == "HP" and op == "+":
             hp = hp + val
             if hp >= maxHP:
                 dbs.updateStat(stat, maxHP, "set")
             else:
                 dbs.updateStat(stat, val, "inc")
+
+        # if the effect wants to decrease HP, make sure it won't go below 0
         elif stat == "HP" and op == "-":
             hp = hp - val
             if hp <= 0:
@@ -288,35 +369,51 @@ def useItem(name):
                 return message
             else:
                 dbs.updateStat(stat, val, "dec")
+
+        # if the effect increases MP, make sure it won't go above the max
         elif stat == "MP" and op == "+":
             mp = mp + val
             if mp >= maxMP:
                 dbs.updateStat(stat, maxMP, "set")
             else:
                 dbs.updateStat(stat, val, "inc")
+
+        # if the effect decreases MP, make sure it doesn't go past 0
         elif stat == "MP" and op == "-":
             mp = mp - val
             if mp <= 0:
                 dbs.updateStat(stat, 0, "set")
             else:
                 dbs.updateStat(stat, val, "dec")
+
+        # if the effect increases XP, run the gainXP() function
         elif stat == "XP" and op == "+":
             lvlMsg = gainXP(val)
+
+        # otherwise, the effect should buff a stat in battle
         elif stat in [ "MAG", "ATK", "DEF" ]:
             message = [ "TODO: use the item during battle", "MAGENTA" ]
             return message
+
+        # catch exceptions
         else:
             raise Exception("ITEM ERROR: The item effect combo shouldn't exist")
+
+        # setup the message
         message = [ "Ted consumes the {0}! Effect: {1} {2}{3}\n{4}".format(name, stat, op, str(val), lvlMsg), "GREEN" ]
+
         # remove the item from inventory and write the new information to screen
         dbs.updateInv(name, "del")
         dbs.getStats()
         dbs.getInventory()
-    # TODO try something with key items
+
     # otherwise, let the user know it can't be used
     else:
         message = [ "Ted, you can't use this item'!", "RED" ]
+
+    # return the message
     return message
 
+# function to exit the game
 def endGame():
     sys.exit()
